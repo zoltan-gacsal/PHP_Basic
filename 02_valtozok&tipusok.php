ea1c7349e4d13ea9154db39353b7bed6517ba84b
zoltan-gacsal <zoltan.gacsal87@gmail.com>
2024-05-27 14:09:32 +0200

    02_valtozok&tipusok

---
+++ 02_valtozok&tipusok.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+PHP Tipusok
+
+Integer - Egész számok
+Float - Lebegő pontos számok
+String - Karakterláncok
+Boolean - True|False
+Array - Tömb amely több értéket képes tárolni
+NULL - Az érték hiányát jelzi
+Object - Objektum példány egy felhasználó által definiált osztályból
+Resource - Az erőforrás egy speciális változó, amely egy külső erőforrásra hivatkozik (megnyitott fájlok, adatbázis kapcsolatok)
+
+*/ 
+
+$eletkor; // ez deklaráció  
+$nev = "zsuzsi"; // ez definíció
+
+
+//integer
+$intSzam1 = 55;
+$intSzam2 = -142;
+
+//float
+$floatSzam1 = 3.14;
+$floatSzam2 = 45.6332;
+
+//string
+$szoveg1 = "Ez a php alapozo";
+$szoveg2 = "hát ez az";
+
+//boolean
+$nagykoru = true;
+$kiskoru = false;
+
+//array
+$nevek = ["Zsuzsi", "zoli"];
+$korok = array(19, 23, 34, "560", true, null);
+
+//NULL
+$nincsErtek;
+$nincsErtek2 = null;
+
