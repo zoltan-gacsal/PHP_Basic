2e1630a50022744992c1baf23c6bd14b1d327f2e
zoltan-gacsal <zoltan.gacsal87@gmail.com>
2024-05-27 14:17:41 +0200

    05_aritmetikai_operatorok

---
+++ 05_aritmetikai_operatorok.php
@@ -0,0 +1,15 @@
+<?php
+
+//Aritmetikai operátorok
+
+// operátorok:    + - * / % ** 
+
+echo 5 + 7 . PHP_EOL;
+echo 5 - 7 . PHP_EOL;
+echo 5 * 7 . PHP_EOL;
+echo 5 / 7 . PHP_EOL;
+echo 7 % 5 . PHP_EOL;
+echo 2 ** 4 . PHP_EOL;
+
+// Aritmetikai operátor elsőbbség - operator presedence
+// sorrendben igy néz ki: () ** * / % + -
