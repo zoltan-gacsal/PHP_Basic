38948044672cb45d0e7b0baafdbfb57f8a6cf988
zoltan-gacsal <zoltan.gacsal87@gmail.com>
2024-05-27 14:18:58 +0200

    06_ertekado_operatorok

---
+++ 06_ertekado_operatorok.php
@@ -0,0 +1,14 @@
+<?php
+
+//Hozzárendelési (Értékadó) operátorok - Assigment operators
+
+// operátorok:  =  +=  -=  *=  /=  %=
+
+$szam = 5;  // 5
+$szam += 1;  // 6
+$szam -= 2;  // 4
+$szam *= 2;  // 8
+$szam /= 2;  // 4
+$szam %= 2;  // 0
+
+echo $szam . PHP_EOL;
